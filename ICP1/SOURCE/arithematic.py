de8c0f5a7c6cf324b27871a567c87806299fbf6e
rishitha  bobba <rb79f@mail.umkc.edu>
2019-01-25 16:34:53 -0600

    2nd question

---
+++ ICP1/SOURCE/arithematic.py
@@ -0,0 +1,12 @@
+a=int(input("enter a value:"))
+b= int(input("enter b value:"))
+c=a+b
+print("sum of a and b is: %d"%c)
+d=a-b
+print("subtraction of a and b is: %d"%d)
+e=a*b
+print("multiplication of a and b is: %d"%e)
+f=a//b
+print("division of a and b is:%d"%f)
+g=a%b
+print("modular division of a and b is: %d"%g)
